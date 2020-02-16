dda71a584373db5560857c8a3c90d22e6e050b26
BlueJTG <timothyiangarcia@gmail.com>
2020-02-15 23:14:28 -0500

    Edit Camera Controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,12 +7,14 @@ public class CameraController : MonoBehaviour
     public PlayerController myPlayer;
     private Vector3 playerPosition;
     private float distanceToMove;
+    private Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
         myPlayer = FindObjectOfType<PlayerController>();
         playerPosition = myPlayer.transform.position;
+        cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -21,5 +23,16 @@ public class CameraController : MonoBehaviour
         distanceToMove = myPlayer.transform.position.x - playerPosition.x;
         transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
         playerPosition = myPlayer.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            cam.orthographicSize = 3.5f;
+            transform.position = new Vector3((transform.position.x + distanceToMove) - 3, transform.position.y - 2, transform.position.z);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            cam.orthographicSize = 5.0f;
+            transform.position = new Vector3((transform.position.x + distanceToMove) + 3, transform.position.y + 2, transform.position.z);
+        }
     }
 }
